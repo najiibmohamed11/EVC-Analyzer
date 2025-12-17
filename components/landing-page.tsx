7dd022929653ed05df0f7542ae0db8a359caebb1
najiibmohamed11 <abdinajibmohamed304@gmail.com>
2025-12-17 07:16:18 +0300

    change markating title and add subtitle

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -19,11 +19,20 @@ export function UploadPdfClient() {
             transition={{ duration: 0.5, delay: 0.1 }}
             className="text-5xl md:text-6xl font-serif font-medium text-gray-900 tracking-tight mb-4"
           >
-            Make a tailored
+            Analyze EVC & Salaam Bank
             <br />
-            course for you
+              transactions
           </motion.h1>
         </div>
+          <motion.p
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+            className="text-gray-500 text-lg max-w-md mx-auto font-light text-center"
+          >
+            Upload Waafi activity PDFs to analyze 
+            <br />EVC & Salaam Bank transactions
+          </motion.p>
         {/* Upload Zone */}
         <ConnectedUpload />
       </div>
